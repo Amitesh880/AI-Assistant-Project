6fd24f447f0a172562654caeae999ec3d57537a2
Amitesh880 <amitesh.2327cse1281@kiet.edu>
2026-05-10 02:17:40 +0530

    feat: initialize express server and implement JWT authentication middleware

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"https://ai-assistant-project-ie1k.onrender.com",
+    origin: ["https://ai-assistant-project-ie1k.onrender.com", "http://localhost:5173"],
     credentials:true
 }))
 const PORT=process.env.PORT || 8000
